29daaf1b55494db25b4dc55605b4fcd93e9a28a6
Jordan Kasper <jordan@jordankasper.com>
2017-10-15 13:28:19 -0400

    Added a "live" demo script

---
+++ test/live.js
@@ -0,0 +1,10 @@
+
+let weather = require('../src/weather')({
+    apiKey: process.env.WEATHER_API_KEY,
+    location: {
+        lat: 38.9649734,
+        lng: -77.0207249
+    }
+});
+
+weather().then((data) => console.log(data)).catch(console.error);
