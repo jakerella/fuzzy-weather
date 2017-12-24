5446523cf5ae23f7857d041023b9a3d6cb5522d6
Jordan Kasper <jordan@jordankasper.com>
2017-12-24 13:56:06 -0500

    Fixed up temperature output for downward heading days

--- src/conditions/temp.js
+++ src/conditions/temp.js
@@ -36,7 +36,7 @@ high of ${Math.round(data.temperatureMax)} degrees peaking around ${peak.format(
     return output;
 }
 
-function getHourlyText(data, timezone) {
+function getHourlyText(data, timezone, dailyData) {
     let text = [];
 
     let xValues = [];
@@ -45,6 +45,12 @@ function getHourlyText(data, timezone) {
     let maxHour = 0;
     let minTemp = 200;
     let minHour = 23;
+
+    let dailyMaxTime = moment.tz(dailyData.temperatureMaxTime * 1000, 'GMT').tz(timezone);
+    let dailyMinTime = moment.tz(dailyData.temperatureMinTime * 1000, 'GMT').tz(timezone);
+
+    console.log(dailyData.temperatureMinTime, dailyData.temperatureMaxTime);
+    debug('max/min', dailyMinTime.format('h'), dailyMaxTime.format('h'));
 
     data.forEach(function determineInstances(hourData, i) {
         let hour = moment.tz(hourData.time * 1000, 'GMT').tz(timezone);
@@ -62,51 +68,45 @@ function getHourlyText(data, timezone) {
     });
 
     if (xValues.length) {
-        // Use linear regression (least squares) to determine if rain
-        // chances increase or decrease through the day. Check the fit of the
-        // line to ensure correlation is strong enough in either direction
-        let regrData = {};
-        let regr = lsq(xValues, yValues, true, regrData);
-        let xMin = xValues[0];
-        let xMax = xValues[xValues.length-1];
-
-        debug(regr(xMin), regr(xMax), regrData);
+        // TODO...
+        // Using linear regression on this temp data does not appear to be giving
+        // good results, especially when the requested time (hour) is later in the
+        // day. For now, I'm going to manually look at peak hours and try to
+        // determine if this is a typical temp curve day or not.
 
         let maxHours = moment.tz(data[0].time * 1000, 'GMT').tz(timezone);
         maxHours.add(maxHour, 'h');
         let minHours = moment.tz(data[0].time * 1000, 'GMT').tz(timezone);
         minHours.add(minHour, 'h');
 
-        if (regrData.bErr < 0.05 && regrData.mErr < 0.005) {
-            if (regr(xMin) < regr(xMax) && (regr(xMax) - regr(xMin)) > 0.4) {
-                text.push(
-`The temperature is currently ${Math.round(yValues[0])} degrees and will be climbing
-through {day}, peaking at about ${Math.round(maxTemp)} degrees around ${maxHours.format('h a')}.`
-                );
+        if (dailyMaxTime.format('h') > 17) {
+            text.push(
+`The temperature is currently ${Math.round(yValues[0])} degrees and {day} it will be climbing
+through the evening, peaking at about ${Math.round(maxTemp)} degrees around ${maxHours.format('h a')}.`
+            );
 
-                if (xValues[0] < 17) {
-                    xValues.forEach(function findSixPM(hour, i) {
-                        if (Number(hour) === 17) {
-                            text.push(`It'll be about ${Math.round(yValues[i])} for your commute home at 5 pm`);
-                        }
-                    });
-                }
-
-            } else if (regr(xMin) > regr(xMax) && (regr(xMin) - regr(xMax)) > 0.4) {
-                text.push(
-`The temperature is currently ${Math.round(yValues[0])} degrees and will be heading
-down through {day}, it'll get down to about ${Math.round(minTemp)} degrees by ${minHours.format('h a')}.`
-                );
-
-                if (xValues[0] < 17) {
-                    xValues.forEach(function findCommuteTime(hour, i) {
-                        if (Number(hour) === 17) {
-                            text.push(`It'll be about ${Math.round(yValues[i])} for your commute home at 5 pm`);
-                        }
-                    });
-                }
-
+            if (xValues[0] < 17) {
+                xValues.forEach(function findSixPM(hour, i) {
+                    if (Number(hour) === 17) {
+                        text.push(`It'll be about ${Math.round(yValues[i])} at the end of the work day`);
+                    }
+                });
             }
+
+        } else if (dailyMaxTime.format('h') < 12) {
+            text.push(
+`The temperature is currently ${Math.round(yValues[0])} degrees, but temps will be heading
+down through {day}, it'll get down to about ${Math.round(minTemp)} degrees at ${minHours.format('h a')}.`
+            );
+
+            if (xValues[0] < 17) {
+                xValues.forEach(function findCommuteTime(hour, i) {
+                    if (Number(hour) === 17) {
+                        text.push(`It'll be about ${Math.round(yValues[i])} at the end of the work day`);
+                    }
+                });
+            }
+
         } else {
             text.push(
 `It's currently ${Math.round(yValues[0])} degrees outside. You'll see a high of about
@@ -116,7 +116,7 @@ ${Math.round(maxTemp)} degrees around ${maxHours.format('h a')}.`
             if (xValues[0] < 17) {
                 xValues.forEach(function findSixPM(hour, i) {
                     if (Number(hour) === 17) {
-                        text.push(`It'll be about ${Math.round(yValues[i])} for your commute home at 5 pm`);
+                        text.push(`It'll be about ${Math.round(yValues[i])} at the end of the work day`);
                     }
                 });
             }
--- test/specs/temp.spec.js
+++ test/specs/temp.spec.js
@@ -2,6 +2,7 @@
 
 let chai = require('chai'),
     temp = require('../../src/conditions/temp'),
+    moment = require('moment-timezone'),
     weatherDataGenerate = require('../data/dc.weather');
 
 let now = new Date();
@@ -44,15 +45,46 @@ describe('temp module', function() {
             let data = weatherDataGenerate(null, {
                 maxTemp: 85,
                 minTemp: 55,
+                dayPeakHour: 14,
                 heatIndexPercent: 0.05,
                 conditions: []
-            }, '2018-07-01T00:01:00');
+            }, '2018-07-01T07:01:00');
             let tempHourly = data.hourly.data.slice(0,24);
 
-            let result = temp.hourlyText(tempHourly, data.timezone);
+            let result = temp.hourlyText(tempHourly, data.timezone, data.daily);
             expect(result).to.be.a('string')
-                .and.contain('high of about 85 degrees around 3 pm')
-                .and.contain('for your commute home at 5 pm');
+                .and.contain('currently 66')
+                .and.contain('high of about 85 degrees around 2 pm')
+                .and.contain('81 at the end of the work day');
+        });
+
+        it('should be able to get hourly temp info (dropping temps)', function() {
+            let data = weatherDataGenerate(null, {
+                maxTemp: 75,
+                minTemp: 40,
+                dayPeakHour: 8,
+                dayCurve: 'down',
+                heatIndexPercent: 0,
+                conditions: []
+            }, '2018-07-01T07:01:00');
+            let tempHourly = data.hourly.data.slice(0,24);
+
+            let dailyData = {};
+            data.daily.data.forEach(function(singleDayData) {
+                if (moment(singleDayData.time * 1000).format('YYYY-MM-DD') === '2018-07-01') {
+                    dailyData = singleDayData
+                    singleDayData.type = 'daily';
+                }
+            });
+
+            // console.log(tempHourly.map(d=>Math.round(d.temperature)));
+
+            let result = temp.hourlyText(tempHourly, data.timezone, dailyData);
+            expect(result).to.be.a('string')
+                .and.contain('currently 73')
+                .and.contain('heading down')
+                .and.contain('down to about 40 degrees at 11 pm')
+                .and.contain('54 at the end of the work day');
         });
 
     });
